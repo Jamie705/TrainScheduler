fd1cad8b62c09dd036f4ba5ec87ad21c04658bae
myusername <rodriguez.jamie@live.com>
2018-10-02 00:05:53 -0400

    updated css, added delete button in js

--- assets/javascript/fire.js
+++ assets/javascript/fire.js
@@ -1,12 +1,5 @@
 $(document).ready(function () {
 
-//variables
-var name = "";
-var destination = "";
-var firstTrainTime = 0;
-var frequency = 0;
-
-
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyB5CJq1Y8RFHTzN3yIpI2HSyCAvO-z0vCk",
@@ -16,6 +9,7 @@ var config = {
     storageBucket: "trainschedule-8b3fa.appspot.com",
     messagingSenderId: "346094434969"
 };
+
 
 firebase.initializeApp(config);
 // Create a variable to reference the database.
@@ -28,15 +22,17 @@ var database = firebase.database();
         // Grabbed values from text boxes
         var name = $("#name-input").val().trim();
         var destination = $("#destination-input").val().trim();
-        var firstTrainTime = moment($("#firstTrain-input").val().trim());
+        var firstTrainTime = $("#firstTrain-input").val().trim();
         var frequency = $("#frequency-input").val().trim();
 
-    //Create local object
+        //Create local object
     var newTrain = {
         name: name,
         destination: destination,
         firstTrainTime: firstTrainTime,
         frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP,
+
     };
         // Code for handling the push
         database.ref().push(newTrain);
@@ -53,6 +49,10 @@ var database = firebase.database();
         $("#firstTrain-input").val("");
         $("#frequency-input").val("");
 
+        //Handle the errors
+        // }, function (errorObject) {
+        //     console.log("Errors handled: " + errorObject.code);
+        
     });
 
     // Firebase watcher .on("child_added"
@@ -72,24 +72,67 @@ var database = firebase.database();
         console.log(firstTrainTime);
         console.log(frequency);
 
+    // +++++++++++++++++++++++++Time++++++++++++++
         // Calculate the time before the next train
-        //create a loop that adds frequency minutes to current and create nexttrain time
-        var nextTrain = moment().diff(moment(firstTrainTime, "hours"), "minutes");
-        console.log(nextTrain);
+        // get values from frequency
+        timefrequency = frequency;
         
+        // // Time is 3:30 AM
+        firstTime = moment(firstTrainTime, "hh:mm");
+        console.log(firstTime);
+
+        // First Time (pushed back 1 year to make sure it comes before current time)
+        var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+        console.log("first time converted: " + firstTimeConverted);
+
+        // Current Time
+        var currentTime = moment();
+        console.log("Current time: " + moment(currentTime).format("hh:mm"));
+
+        // Difference between the times
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        console.log("Time difference: " + diffTime);
+
+        // Time differenced and frequency
+        var tRemainder = diffTime % timefrequency;
+        console.log("Time remainder: " + tRemainder);
+
+        // Minute Until Train
+        var tMinutesTillTrain = timefrequency - tRemainder;
+        console.log("Minutes till train " + tMinutesTillTrain);
+
+        // Next Train - adds minutes until next train and formats it in minutes
+        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        console.log("Next Train: " + moment(nextTrain).format("hh:mm"));
+
+        //format next train in hours minutes
+        var nextTrainFormated = moment(nextTrain).format("hh:mm");
+        console.log(nextTrainFormated);
+    // // +++++++++++++++++++++++++Time++++++++++++++
+
         //create new row
-        var newRow = $("<tr>").append(
+        var newRow = $("<tr>").attr("data",name).append(
             $("<td>").text(name),
             $("<td>").text(destination),
             $("<td>").text(firstTrainTime),
-            $("<td>").text(frequency)
+            $("<td>").text(frequency),
+            $("<td>").text(nextTrainFormated),
+            $("<td>").append($("<button>Delete</button>").attr("id", "delete")),
         );
         // Append the new row to the table
         $("#train-table > tbody").append(newRow);
     });
+    // When a user clicks a deletbutton then delete train
+        $("table").on("click", "#delete", function () {
+        $(this).closest("tr").remove();
+        key = $(this).closest("tr").attr("data");
+        console.log(key);
+        database.ref().child(key).remove();
 
-    //     // Handle the errors
-    // }, function (errorObject) {
-    //     console.log("Errors handled: " + errorObject.code);
-    // });
+        });
+
+        // Handle the errors
+    }, function (errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+   
 });
